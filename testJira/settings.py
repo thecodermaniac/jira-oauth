96068f8e7a182b70fe70e67799e5803bc0d3f589
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2024-06-13 23:20:16 +0530

    sec changed

--- testJira/settings.py
+++ testJira/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ck^w#$742ud$--15+givvjadhl52-jpgvs8we3%5+hm5_km9zr'
+SECRET_KEY = 'use your django secrect key '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
